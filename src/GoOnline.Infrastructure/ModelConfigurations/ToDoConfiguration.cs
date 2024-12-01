e5540ecb8245b17254647df5a7679551095d9035
Tomasz <trnosal@gmail.com>
2024-12-01 14:56:05 +0100

    Add ToDoConfiguration

---
+++ src/GoOnline.Infrastructure/ModelConfigurations/ToDoConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using GoOnline.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GoOnline.Infrastructure.ModelConfigurations;
+
+internal class ToDoConfiguration : IEntityTypeConfiguration<ToDo>
+{
+    public void Configure(EntityTypeBuilder<ToDo> builder)
+    {
+        builder.Property(x => x.Title).IsRequired().HasMaxLength(100);
+        builder.Property(e => e.Complete).HasPrecision(3, 2);
+    }
+}
