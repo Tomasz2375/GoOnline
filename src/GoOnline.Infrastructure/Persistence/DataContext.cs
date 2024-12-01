c4167851c02f33906e0652334a4d486ecca00bde
Tomasz <trnosal@gmail.com>
2024-12-01 13:38:00 +0100

    Add db context

---
+++ src/GoOnline.Infrastructure/Persistence/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using GoOnline.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoOnline.Infrastructure.Persistence;
+
+public class DataContext : DbContext, IDataContext
+{
+    public DataContext()
+    {
+    }
+
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+    {
+    }
+
+    public new virtual DbSet<TEntity> Set<TEntity>()
+        where TEntity : class, IEntity
+    {
+        return base.Set<TEntity>();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+}
