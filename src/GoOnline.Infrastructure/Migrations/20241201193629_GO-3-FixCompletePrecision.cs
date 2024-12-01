d3f9e089149df7328f721300ab02dafd2d12b49c
Tomasz <trnosal@gmail.com>
2024-12-01 20:59:33 +0100

    Create migration GO-3

---
+++ src/GoOnline.Infrastructure/Migrations/20241201193629_GO-3-FixCompletePrecision.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoOnline.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class GO3FixCompletePrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Complete",
+                table: "ToDo",
+                type: "decimal(5,2)",
+                precision: 5,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(3,2)",
+                oldPrecision: 3,
+                oldScale: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Complete",
+                table: "ToDo",
+                type: "decimal(3,2)",
+                precision: 3,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,2)",
+                oldPrecision: 5,
+                oldScale: 2);
+        }
+    }
+}
